cb3451a8ba7502c1fccef38c06aa147eff3dd071
Loong <2553168654@qq.com>
2024-04-20 13:03:30 +0800

    完成购物车页面数据展示相关接口

--- aurora-server/src/main/java/org/aurora/controller/user/AddressBookController.java
+++ aurora-server/src/main/java/org/aurora/controller/user/AddressBookController.java
@@ -1,5 +1,9 @@
 package org.aurora.controller.user;
 
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.aurora.entity.AddressBook;
+import org.aurora.result.Result;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -11,8 +15,15 @@ import org.springframework.web.bind.annotation.*;
  */
 @RestController
 @RequestMapping("addressBook")
+@Slf4j
+@Api(tags = "C端-地址簿接口")
 public class AddressBookController {
 
+  @PostMapping("")
 
+    public Result<String> add(@RequestBody AddressBook addressBook) {
+        log.info("新增地址:{}", addressBook);
+        return null;
+    }
 }
 
